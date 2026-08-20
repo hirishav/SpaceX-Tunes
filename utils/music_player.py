eb7bc4d2b8fead4ec911c9a8ac70715a32e5ad1b
Rishav <das.rishavdas2005@gmail.com>
2026-08-21 02:28:15 +0530

    Remove manual client overrides

--- utils/music_player.py
+++ utils/music_player.py
@@ -32,7 +32,6 @@ ytdl_format_options = {
     'no_warnings': True,
     'default_search': 'auto',
     'source_address': '0.0.0.0', # bind to ipv4 since ipv6 addresses cause issues sometimes
-    'extractor_args': {'youtube': {'player_client': ['ios', 'tv', 'web']}},
 }
 
 # Resolve absolute path for cookies.txt
