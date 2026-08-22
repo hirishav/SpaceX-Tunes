0d34503e21f9cf86bb0260b89ccfc44909acf9c3
Rishav <das.rishavdas2005@gmail.com>
2026-08-23 00:49:13 +0530

    Lavalink Fixed:

--- utils/music_player.py
+++ utils/music_player.py
@@ -32,6 +32,11 @@ ytdl_format_options = {
     'no_warnings': True,
     'default_search': 'auto',
     'source_address': '0.0.0.0', # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'extractor_args': {
+        'youtube': {
+            'client': ['android', 'ios', 'tv', 'web']
+        }
+    }
 }
 
 # Resolve absolute path for cookies.txt
